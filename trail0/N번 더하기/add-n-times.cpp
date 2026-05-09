986a14e23924756d84e6399b1bab509e65c7fbff
DoroDoro12 <babaeq2011@gmail.com>
2026-05-09 22:23:18 +0900

    [Passed] N번 더하기 (8ms, 0MB)

---
+++ trail0/N번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, n, i;
+    cin >> a >> n;
+
+    for (i=0; i < n; i++) {
+        a += n;
+        cout << a << "\n";
+    }
+    return 0;
+}
