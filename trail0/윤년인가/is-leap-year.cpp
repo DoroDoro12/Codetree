4db38509f2cfae80f7df294ee377c9ad7db35193
DoroDoro12 <babaeq2011@gmail.com>
2026-05-09 22:53:44 +0900

    [Passed] 윤년인가 (8ms, 0MB)

---
+++ trail0/윤년인가/is-leap-year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >> y;
+
+    if (y % 100 == 0 && y % 400 != 0) {
+        cout << "false";
+    }else {
+        if (y % 4 == 0) {
+            cout << "true";
+        }else {
+            cout << "false";
+        }
+    }
+    return 0;
+}
