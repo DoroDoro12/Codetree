609c12a8db406221388e65c22939d4be00bd40c6
DoroDoro12 <babaeq2011@gmail.com>
2026-05-15 12:35:11 +0900

    [Passed] 길이 단위 환산하기 (8ms, 0MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double ft = 30.48, n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << n * ft;
+    return 0;
+}
