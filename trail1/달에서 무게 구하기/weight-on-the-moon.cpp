d52cc5a443338250f74182414cf6434faca9b633
DoroDoro12 <babaeq2011@gmail.com>
2026-05-15 11:26:17 +0900

    [Passed] 달에서 무게 구하기 (8ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(6);
+
+    int w = 13;
+    double g = 0.165;
+
+    cout << w << " * " << g << " = " << w * g;
+    return 0;
+}
