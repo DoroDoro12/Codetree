ee90152f466337bd1d3141d3323ff211504d27f4
DoroDoro12 <babaeq2011@gmail.com>
2026-05-16 15:58:04 +0900

    [Passed] 전화번호 바꾸기 (8ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int z,f, r;
+    char c;
+
+    cin >> z >> c >> f >> c >> r;
+    cout << "010" << "-" << r << "-" << f;
+    return 0;
+}
