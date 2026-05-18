ce5ac388f6cde469996d573f522095c8f6252c19
DoroDoro12 <babaeq2011@gmail.com>
2026-05-18 17:26:24 +0900

    [Passed] A의 B승 (8ms, 0MB)

---
+++ trail1/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    int prod = 1;
+
+    for(int i = 1; i <= b; i++) {
+        prod *= a;
+    }
+
+    cout << prod;
+    return 0;
+}
