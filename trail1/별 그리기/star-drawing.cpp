54b7bc0533bec0ca8b9c9382f832a3144e9c1990
DoroDoro12 <babaeq2011@gmail.com>
2026-05-20 12:54:11 +0900

    [Passed] 별 그리기 (2ms, 0MB)

---
+++ trail1/별 그리기/star-drawing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt = 1;
+
+    for (int i = 0; i < 2*n-1; i++) {
+        for (int j = 0; j < n-((cnt+1)/2); j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < cnt; k++) {
+            cout << "*";
+        }
+        cout << endl;
+        if (i < n-1) {
+            cnt += 2;
+        }else {
+            cnt -= 2;
+        }
+    }
+    return 0;
+}
