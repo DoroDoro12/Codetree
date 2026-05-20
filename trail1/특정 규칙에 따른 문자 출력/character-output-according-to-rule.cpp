e67bc2cdb3241c46d9fc93c34e52a19c73f248cc
DoroDoro12 <babaeq2011@gmail.com>
2026-05-20 13:08:16 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (2ms, 1MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt = 1;
+
+    for(int i = 0; i < 2*n-1; i++) {
+        for (int j = 0; j < 2*(n-1-i); j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < cnt; k++) {
+            cout << "@ ";
+        }
+        cout << endl;
+        if (i < n-1) {
+            cnt++;
+        }else {
+            cnt--;
+        }
+    }
+    return 0;
+}
