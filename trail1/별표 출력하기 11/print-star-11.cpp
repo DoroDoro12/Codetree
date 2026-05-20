171d39365b133b10a2d4ce5d26cc4a228141654f
DoroDoro12 <babaeq2011@gmail.com>
2026-05-20 14:22:33 +0900

    [Passed] 별표 출력하기 11 (2ms, 0MB)

--- trail1/별표 출력하기 11/print-star-11.cpp
+++ trail1/별표 출력하기 11/print-star-11.cpp
@@ -7,15 +7,12 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < 2*n+1; i++) {
-        for (int j = 0; j < 4*n+1; j++) {
-            if (j % 2 == 1) {
-                cout << " ";
-            }else {
-                if (i % 2 == 1 && j % 4 == 2) {
-                    cout << " ";
-                }else {
-                    cout << "*";
-                }
+        for (int j = 0; j < 2*n+1; j++) {
+            if (i % 2 == 0 || j % 2 == 0) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
             }
         }
         cout << endl;
