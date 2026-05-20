19d3a4b469adb3d1c0db6d32d1ccc6cba9fb6767
DoroDoro12 <babaeq2011@gmail.com>
2026-05-20 12:34:38 +0900

    [Passed] 두 개의 직각삼각형 (2ms, 0MB)

---
+++ trail1/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            cout << "*";
+        }
+        for (int k = 0; k < 2*i; k++){
+            cout << " ";
+        }
+        for (int q = 0; q < n-i; q++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
