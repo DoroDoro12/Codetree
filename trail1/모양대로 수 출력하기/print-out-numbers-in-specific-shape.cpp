1d44a60e9499bf54d1b8cd7a9d4baa4db3e6c0d6
DoroDoro12 <babaeq2011@gmail.com>
2026-05-21 15:49:20 +0900

    [Passed] 모양대로 수 출력하기 (2ms, 0MB)

---
+++ trail1/모양대로 수 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "  ";
+        }
+        for (int k = i; k > 0; k--) {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
