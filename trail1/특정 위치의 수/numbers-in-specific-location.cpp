05675fc076732fac5ee854ce090f6aacf7c75c19
DoroDoro12 <babaeq2011@gmail.com>
2026-05-22 14:00:38 +0900

    [Passed] 특정 위치의 수 (2ms, 0MB)

---
+++ trail1/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        int n;
+        cin >> n;
+        arr[i] = n;
+    }
+
+    cout << arr[2] + arr[4] + arr[9];
+    return 0;
+}
