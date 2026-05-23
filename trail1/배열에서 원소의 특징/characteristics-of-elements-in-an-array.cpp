4054c9dcd89c4edd8f0a410691d532026584f446
DoroDoro12 <babaeq2011@gmail.com>
2026-05-23 15:44:58 +0900

    [Passed] 배열에서 원소의 특징 (2ms, 0MB)

---
+++ trail1/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        int n;
+        cin >> n;
+
+        arr[i] = n;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] % 3 == 0) {
+            cout << arr[i-1];
+            break;
+        }
+    }
+    return 0;
+}
