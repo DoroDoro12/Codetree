3f676a105a2a64f202b0ad18e2de29acfd47631e
DoroDoro12 <babaeq2011@gmail.com>
2026-05-25 20:09:25 +0900

    [Passed] 가장 왼쪽에 있는 최댓값 (2ms, 0MB)

--- trail1/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
+++ trail1/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main() {
     // Please write your code here.
-    // 입력할 n개 숫자 입력
     int n;
     int arr[1000];
     cin >> n;
 
-    // n개 숫자 입력한 arr 수열 만들기
     for (int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        arr[i] = a;
+        int num;
+        cin >> num;
+
+        arr[i] = num;
     }
 
-
-    // 최대인덱스 설정
-    int idx = n;
-
+    // n개 정수 가진 수열 어레이 제작완료.
+    int fin = n;
 
     while (1) {
         int max = 0;
-        n = idx;
-        for (int i = 0; i < n; i++) {
-            if (max <= arr[i]) {
+        int idx = 0;
+
+        for (int i = 0; i < fin; i++) {
+            if (max < arr[i]) {
                 max = arr[i];
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (max == arr[i]) {
                 idx = i;
-                break;
             }
         }
 
-        if (idx == 0) {
-            cout << idx+1;
+        fin = idx;
+        cout << fin+1 << " ";
+        if (0 == idx) {
             break;
-        }else {
-            cout << idx+1 << " ";
         }
     }
+
     return 0;
 }
