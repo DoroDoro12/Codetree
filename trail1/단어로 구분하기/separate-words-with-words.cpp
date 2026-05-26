efa075c74a3dbe66a6d0c22a1579ff9d0f8c6fe3
DoroDoro12 <babaeq2011@gmail.com>
2026-05-26 16:20:39 +0900

    [Passed] 단어로 구분하기 (2ms, 1MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i <10; i++) {
+        string str;
+        cin >> str;
+        cout << str << "\n";
+    }
+    return 0;
+}
