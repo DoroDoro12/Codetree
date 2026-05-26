cfcbb588f636216c27f4cda2031e762d848d053d
DoroDoro12 <babaeq2011@gmail.com>
2026-05-26 18:32:18 +0900

    [Passed] 문자열 추가하기 (2ms, 0MB)

---
+++ trail1/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    str += "Hello";
+    cout << str;
+    return 0;
+}
