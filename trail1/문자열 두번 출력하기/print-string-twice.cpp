e625cffde304d08b27258d91eae1d923d7555320
DoroDoro12 <babaeq2011@gmail.com>
2026-05-26 14:23:16 +0900

    [Passed] 문자열 두번 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+
+    for (int i = 0; i < 2; i++) {
+        cin >> str;
+        cout << str << "\n";
+    }
+    return 0;
+}
