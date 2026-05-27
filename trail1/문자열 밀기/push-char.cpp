ab37ac72573390c20dd6cfe857416e508bd9a776
DoroDoro12 <babaeq2011@gmail.com>
2026-05-27 15:43:17 +0900

    [Passed] 문자열 밀기 (2ms, 0MB)

---
+++ trail1/문자열 밀기/push-char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    str = str.substr(1) + str.substr(0, 1);
+    cout << str;
+    return 0;
+}
