ce8137a93d1a7d0ec4cf43aa1b150e680efec152
DoroDoro12 <babaeq2011@gmail.com>
2026-05-27 15:31:33 +0900

    [Passed] e 제거하기 (2ms, 0MB)

---
+++ trail1/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'e') {
+            str = str.substr(0, i) + str.substr(i+1);
+            break;
+        }
+    }
+
+    cout << str;
+    return 0;
+}
