a17251f857e040677d64fc898329c04181f051c5
DoroDoro12 <babaeq2011@gmail.com>
2026-05-27 18:00:27 +0900

    [Passed] 아스키코드표 맞추기 (2ms, 0MB)

---
+++ trail1/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int c;
+    for (int i = 0; i < 5; i++) {
+        cin >> c;
+        cout << char(c) << " ";
+    }
+    return 0;
+}
