76e6016fdf0795d7c03078123bbcae475c9f28cf
DoroDoro12 <babaeq2011@gmail.com>
2026-05-27 18:56:43 +0900

    [Passed] 두 수의 합과 1 (1ms, 0MB)

---
+++ trail1/두 수의 합과 1/two-nums-sum-and-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    string str = to_string(a + b);
+    int cnt = 0;
+
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '1') {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
