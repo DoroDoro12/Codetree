77c25e6a3a0a2bb264ed6da3683e3a291f8eb7be
DoroDoro12 <babaeq2011@gmail.com>
2026-06-02 22:45:30 +0900

    [Passed] 사각형의 총 넓이 2 (2ms, 1MB)

---
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr[201][201] = {};
+
+    for (int i = 0; i < n; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1 += 100;
+        y1 += 100;
+        x2 += 100;
+        y2 += 100;
+
+        for (int j = x1; j < x2; j++) {
+            for (int k = y1; k < y2; k++) {
+                arr[j][k] = 1;
+            }
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < 201; i++) {
+        for (int j = 0; j < 201; j++) {
+            sum += arr[i][j];
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
