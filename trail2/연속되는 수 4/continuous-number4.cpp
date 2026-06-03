f2d2a6c834a87daaf90b0400ce75c1a53225815d
DoroDoro12 <babaeq2011@gmail.com>
2026-06-03 17:19:16 +0900

    [Passed] 연속되는 수 4 (2ms, 0MB)

---
+++ trail2/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int arr[1000];
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        arr[i] = a;
+    }
+
+    int cnt = 0;
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || arr[i] > arr[i-1]) {
+            cnt++;
+            max = (max < cnt) ? cnt:max;
+        }else {
+            cnt = 1;
+        }
+    }
+
+    cout << max;
+    return 0;
+}
