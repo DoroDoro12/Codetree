6a7f173ed3a0092dd3d3f09f99db83e4c8cd86e0
DoroDoro12 <babaeq2011@gmail.com>
2026-06-06 00:03:50 +0900

    [Passed] 거울에 레이저 쏘기 2 (2ms, 0MB)

--- trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ trail2/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
-using namespace std;
-int n, k;
 
-// 배열은 n+2크기로만들고, 인덱스는 1 ~ n
+using namespace std;
+// n은 격자 수, k는 레이저 위치.
+// dir은 레이저 방향, xy는 시작위치.
+int n, k, dir;
+int x, y;
 char arr[1000][1000];
+
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, -1, 0, 1};
 
@@ -12,69 +15,59 @@ bool InRange(int x, int y) {
     return (x >= 0 && x < n && y >= 0 && y < n);
 }
 
-int l_mirror(int d) {
-    if (d == 0) return 1;
-    if (d == 1) return 0;
-    if (d == 2) return 3;
-    return 2;
-}
-
-int r_mirror(int d) {
-    d = 3 - d;
-    return d;
-}
-
-int main() {
-    // Please write your code here.
-    cin >> n;
-    cin.ignore();
-
-    for (int i = 0; i < n; i++) {
-        string s;
-        getline(cin, s);
-        for (int j = 0; j < n; j++) {
-            arr[i][j] = s[j];
-        }
-    }
-    cin >> k;
-    int dir;
-    dir = ((k-1) / n);
-
-    // 시작점 (레이저) x,y
-    int x;
-    int y;
+// 레이저 방향 및 시작 위치 잡기.
+void Initialize() {
+    dir = (k-1) / n;
     if (k <= n) {
-        x = 0; 
+        x = 0;
         y = k-1;
     }else if (k <= 2*n) {
-        x = k - n;
+        x = k - (n + 1);
         y = n - 1;
     }else if (k <= 3*n) {
         x = n - 1;
-        y = n - (k - 2 * n);
+        y = n - (k - (2 * n));
     }else {
-        x = n - (k - 3 * n);
+        x = n - (k - (3 * n));
         y = 0;
     }
+}
 
+void Move() {
+    x += dx[dir];
+    y += dy[dir];
+}
+
+// 빛 비추기 시작.
+int Simulate() {
     int cnt = 0;
-    while (1) {
+    while(InRange(x, y)) {
         if (arr[x][y] == '/') {
-            dir = l_mirror(dir);
-            cnt++;
-        }else if (arr[x][y] == '\\') {
-            dir = r_mirror(dir);
-            cnt++;
+            dir = dir ^ 1;
+        }else {
+            dir = 3 - dir;
         }
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        if (!InRange(nx, ny)) break;
-        else {
-            x = nx;
-            y = ny;
-        }
-    } 
+        Move();
+        cnt += 1;
+    }
+    return cnt;
+}
 
-    cout << cnt;
+
+int main() {
+    // 거울 배열 완성하기
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    //레이저 위치 k받고 초기조건 함수 부르기.
+    cin >> k;
+    Initialize();
+
+    int result = Simulate();
+    cout << result;
     return 0;
 }
